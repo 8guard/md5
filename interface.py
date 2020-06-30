a40565cf512aa060557cee60dd19dff33b4bdbfc
mizzardy <natavalance@yandex.ru.ru>
2020-06-30 17:30:22 +0300

    clearning

--- interface.py
+++ interface.py
@@ -11,20 +11,19 @@ class App(Frame):
         self.entrythingy = Entry()
         self.entrythingy.pack()
 
-        self.sendbutton = Button(command=self.check_md5,text="Hesh out!")
-        self.sendbutton.pack()
+        self.sendhash = Button(command=self.check_md5,text="Hesh out!")
+        self.sendhash.pack()
 
-        self.sendbutton.bind("1", self.check_md5)
+        self.sendhash.bind("1", self.check_md5)
 
 
     def check_md5 (self):
         md5 = decrypt_main()
         self.password = self.entrythingy.get()
         md5.check_password(self.password)
-        print(md5.hash_get)
 
 new_app = App()
-new_app.master.title("md5decrypt")
+new_app.master.title("hesh#sakura v0.1")
 new_app.master.minsize(250, 50)
 new_app.master.resizable(width=False, height=False)
 new_app.mainloop()
--- md5decrypt.py
+++ md5decrypt.py
@@ -8,20 +8,22 @@ class decrypt_main:
     def check_password(self, hash_get):
         #пробуем прочитать файл с паролями
         try:
-            pswdfile = open('password.txt','r')
+            pswdfile = open('hashes.txt','r')
             #начинаем генерировать хэши из файла (построчно)
             for password in pswdfile:
                 gen_hash = hashlib.md5(password.strip().encode('utf-8')).hexdigest()
-                print(password)
+
                 #справниваем хэши, при удачном совпадении, выводим результат
                 if hash_get == gen_hash:
-                    print('пароль найден :', password)
-                    dump_jfile = 'password.json'
+                    print('\nхэш подобран, его значение :', password)
+                    dump_jfile = 'validhash.json'
                     with open(dump_jfile, 'w') as pws_obj:
-                        json.dump(password, pws_obj)
+                        json.dump("hash : " + hash_get + " значение: " + password, pws_obj, ensure_ascii=False)
                         break
+                elif hash_get == "none":
+                    print("вы не ввели хэш")
             else :
-                print("не нашли пароль")
+                print("хэш не найден")
         except:
-            print("\nфайл password.txt не найден\n")
-            print("пожалуйста создайте файл с паролями\n")
+            print("\nфайл hashes.txt не найден\n")
+            print("пожалуйста создайте файл с хешами\n")
